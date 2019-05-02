1f96a34ba5a065ad533366051596985ff3b7861a
Ata <ahakcil@gmail.com>
2019-05-02 05:36:14 +0200

    Minimal viable version
    Its working, but working barely

--- background.js
+++ background.js
@@ -1,3 +1,37 @@
-chrome.runtime.onInstalled.addListener(function() {
-    // add an action here
-});
+var _active="false", _hostname="localhost";
+
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+    	if(_active=="false"){return {requestHeaders: details.requestHeaders};}
+		details.requestHeaders.push({
+	        name: 'Host',
+	        value: _hostname
+	    });
+          return {requestHeaders: details.requestHeaders};
+    },
+
+    {urls: ["<all_urls>"]},
+    ["blocking", "requestHeaders"]
+);
+
+
+//chrome.webRequest.onBeforeRequest.addListener(
+
+
+
+chrome.extension.onConnect.addListener(function(port) {
+      console.log("Connected .....");
+      port.postMessage(String(_active) + "," + _hostname)
+      port.onMessage.addListener(function(msg) {
+      		console.log(msg)
+           	var passed = msg.split("===");
+           	if(passed[0]==="active"){
+           		_active = passed[1]
+           	}else if(passed[0]==="host"){
+           		_hostname = passed[1]
+           	}
+        console.log(_active)
+        console.log(_hostname)
+      });
+ })
--- content.js
+++ content.js
@@ -1,1 +1,53 @@
-alert("test");
+var _hostname = "localhost"
+var _active = "false";
+
+
+ var port = chrome.extension.connect({
+      name: "Variable highway"
+ });
+
+ port.onMessage.addListener(function(msg){
+ 	console.log(msg)
+ 	msg = msg.split(',');
+ 	_active = msg[0];
+ 	_hostname = msg[1];
+ 	console.log(_active)
+ 	if(_active=="true") _extEnable();
+ 	$("#host-field").val(_hostname);
+ })
+
+function setHost(){
+	_hostname = $("#host-field").val();
+	port.postMessage("host===" + _hostname);
+	window.close();
+}
+
+$(document).ready(function(){
+	document.getElementById('ext-btn').onclick = function(){toggleExtension();}
+	document.getElementById('ok-btn').onclick = function(){setHost();}
+})
+
+function toggleExtension(){
+	if(_active=="false"){
+		_extEnable();
+	}else{
+		_extDisable()
+	}
+	 port.postMessage("active===" + String(_active));
+}
+
+function _extEnable(){
+	console.log("Enabling")
+	_active=true;
+	$("#ext-btn").addClass("ext-enabled");
+	$("#ext-btn").removeClass("ext-disabled");
+	$("#ext-btn").html("Disable")
+}
+
+function _extDisable(){
+	console.log("Disabling")
+	_active=false;
+	$("#ext-btn").removeClass("ext-enabled");
+	$("#ext-btn").addClass("ext-disabled");
+	$("#ext-btn").html("Enable");
+}
